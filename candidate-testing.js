479e0e4f05e9eba0065a68c1f7d378fcfff1e96b
Dhana bayya <krishnabayya@Dhanas-mac-Air.lan>
2024-05-17 09:32:31 -0500

    Part2

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,9 +12,13 @@ let candidateAnswer="";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions =["Who was the first American woman in space? ", 
+"True or false: 5 kilometer == 5000 meters? ",
+"(5 + 3)/2 * 10 = ? ",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? "];
+let correctAnswers =["Sally Ride","true","40","Trajectory","3"];
+let candidateAnswers =[];
 
 
 function askForName() {
@@ -25,6 +29,10 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 candidateAnswer=input.question(question);
+for(let i = 0; i < questions.length; i++){
+  candidateAnswers[i]=input.question(questions[i]);
+  candidateAnswers.push(candidateAnswers[i]);
+}
 
 }
 
@@ -36,6 +44,13 @@ if(correctAnswer===candidateAnswer){
   console.log(`Hey ${candidateAnswer} is rigth answer`);
 }else{
   console.log(`Hey ${candidateAnswer} is wrong answer`);
+}
+for(let i = 0; i < candidateAnswers.length; i++){
+  if(correctAnswers[i] === candidateAnswers[i]){
+    console.log(`Hey ${candidateAnswers[i]} is rigth answer`);
+  }else{
+    console.log(`Hey ${candidateAnswers[i]} is wrong answer`);
+  }
 }
 
 
